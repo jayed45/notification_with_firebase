10eaae059a96a2738ac1944a1a78d415baa92374
jayed45 <jayedhasan45@gmail.com>
2024-04-09 15:01:21 +0600

    notif_from_diff_device

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_con/notification_services.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -31,6 +34,36 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Page"),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            notificationServices.getDeviceToken().then((value) async {
+              var data = {
+                'to': 'f9n5CjljQ3Oyl0OMe08M6L:APA91bEPcbUPe2KNSkh5cQyHrvpGmFQTj43CufdZEmaxOksWknkZK2lH7nVQjcWvr309za0LFqukE6ZnqYJbcw4rdevzRht0yZ8EPheWM8uSDUL0BEoFxySjkOQfqm5iThLZ7axBk43j',
+                'priority': 'high',
+                'notification': {
+                  'title': 'Notification',
+                  'body': 'Sending Notification to other device',
+                },
+                'data': {
+                  'type': 'message',
+                  'id': '1234',
+                  // Add extra parameter
+                }
+              };
+              await http.post(
+                Uri.parse('https://fcm.googleapis.com/fcm/send'),
+                body: jsonEncode(data),
+                headers: {
+                  'Content-Type': 'application/json; charset=UTF-8',
+                  'Authorization': 'key=AAAA58WiiAw:APA91bGJgJMTqKPBmthCus2NFGFEgw42-DbB4oIb0ncdipO95MTOb44cYEtrb0Nes_DoCLdpAaL5HV7NmsIEIO2Yp3fRFMFr8zP9ghtdeXvG11OSKn9tlvzmdqXuaTLDlHPCxdaYWDUV',
+                },
+              );
+            });
+          },
+          child: Text('Send Notification'),
+        ),
       ),
     );
   }
